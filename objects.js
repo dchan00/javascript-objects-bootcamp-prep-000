ebdb922643910df7e0742462904c85d05877e15c
DANIEL CHAN <dchan513@msn.com>
2017-09-15 02:13:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {rita:"your song"}
 
-function updatePlaylist(object, name, song) {
-  object.name=song
+function updatePlaylist(playlist, artistName, songTitle) {
+  object.artistName=songTitle
   return object
 }
