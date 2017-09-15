506d3f6aaacbe683900c7f24df6f622850e0646a
DANIEL CHAN <dchan513@msn.com>
2017-09-15 02:14:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {rita:"your song"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  object.artistName=songTitle
-  return object
+  playlist.artistName=songTitle
+  return playlist
 }
